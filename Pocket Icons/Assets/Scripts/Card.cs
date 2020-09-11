fff4555df996887fecc6738faf5dd0927a236367
Casimir Kuusela <crk.kuusela@gmail.com>
2020-09-11 21:13:03 +0300

    Update Card.cs and Cards.json

--- Pocket Icons/Assets/Scripts/Card.cs
+++ Pocket Icons/Assets/Scripts/Card.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int ReputationCost;
+    public int Power;
+    public bool IsBlessing;
+    public string Title;
+    public string Body;
+    public Image PowerImage;
+    
+    public Card(int rep, int power, bool blessing, string title, string body)
     {
-        
-    }
+        ReputationCost = rep;
+        Power = power;
+        IsBlessing = blessing;
+        Title = title;
+        Body = body;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(IsBlessing)
+        {
+            PowerImage.color = Color.green;
+        }
+        else
+        {
+            PowerImage.color = Color.red;
+        }
     }
 }

5976b82b65064c0dd06e0134bd8f291d86636ee0
Casimir Kuusela <crk.kuusela@gmail.com>
2020-09-11 21:33:31 +0300

    wip

--- Pocket Icons/Assets/Scripts/Cardpack.cs
+++ Pocket Icons/Assets/Scripts/Cardpack.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Cardpack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int CardsInDeck;
+    List<Card> CardList;
+    
+    public Cardpack()
     {
-        
+        CardList.Shuffle();
     }
 }
